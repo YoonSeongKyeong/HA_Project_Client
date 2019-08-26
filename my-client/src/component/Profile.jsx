9a10aae17450917270f3512c12c7963586b62a2a
mdy6099 <mdy6099@naver.com>
2019-08-26 21:55:35 +0900

    image ok

--- my-client/src/component/Profile.jsx
+++ my-client/src/component/Profile.jsx
@@ -19,20 +19,15 @@ class Profile extends Component {
     componentDidMount() {
         let targetUrl1 = 'https://koreanjson.com/users/' + this.props.match.params.id
         fetch(targetUrl1).then(response => response.json()).then(result => (this.setState({user:result})))
-        // let targetUrl2 = 'https://randomuser.me/api/portraits/men/64.jpg'// 그림 관련
-        // const proxyurl = "https://cors-anywhere.herokuapp.com/";
-        // fetch(proxyurl+targetUrl2)
-        // .then(data =>{
-        //     var binaryData = [];
-        //     binaryData.push(data);
-        //     return window.URL.createObjectURL(new Blob(binaryData, {type: "image/jpeg"}))})
-        // .then(ret=>this.setState({image:ret.slice(5)}))
     }
 
     render() {
         return (
             <div>
                 <BackButton history={this.props.history} />
+                <div>
+                    <img src="https://randomuser.me/api/portraits/men/64.jpg" alt=""/>
+                </div>
                 <div>
                     <span>이름</span> <span>{this.state.user.name}</span>
                 </div>
